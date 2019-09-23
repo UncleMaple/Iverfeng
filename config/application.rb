f1e166dbcd7c47f28157194be9142cb0bd01c341
lihaifeng <haifengli@miliantech.com>
2019-09-23 17:54:26 +0800

    添加google邮箱配置

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,20 @@ module SampleApp
     # 在使用 Ajax 处理的表单中添加真伪令牌
     config.action_view.embed_authenticity_token_in_remote_forms = true
 
+    config.action_mailer.default_url_options = { host: '0.0.0.0::3000' }
+
+    ActionMailer::Base.delivery_method = :smtp
+    # gmail
+    ActionMailer::Base.smtp_settings = {
+        :address              => 'smtp.gmail.com',
+        :port                 => 587,
+        :domain               => 'gmail.com',
+        :user_name            => 'skipmaple@gmail.com',
+        :password             => 'wnewepcdphknfjed',
+        :authentication       => :login,
+        :enable_starttls_auto => true
+    }
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
