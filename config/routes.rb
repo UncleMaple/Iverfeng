56e2b2c8484f02ecd3c1d3ec4b8ef4927b0cc381
lihaifeng <haifengli@miliantech.com>
2019-09-27 18:52:52 +0800

    添加动漫风格表格和pagemap导航测试

--- config/routes.rb
+++ config/routes.rb
@@ -19,4 +19,5 @@ Rails.application.routes.draw do
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
   resources :relationships,       only: [:create, :destroy]
+  get '/test', to: 'static_pages#test'
 end
